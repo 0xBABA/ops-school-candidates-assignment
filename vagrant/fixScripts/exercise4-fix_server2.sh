bd273edd1e4f33cd85706c94f09697c4f50abfc6
yoadl <yoadl@crispify.io>
2021-07-28 10:11:56 +0300

    Submitting my solutions for the ops-school candidates assignment

--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 #add fix to exercise4-server2 here
+
+echo '192.168.100.10 server1 server1' | sudo tee -a /etc/hosts
+
+
+# deploy the key
+# echo "deploying key on server2"
+cp /vagrant/id_rsa /home/vagrant/.ssh/
+chown vagrant /home/vagrant/.ssh/id_rsa
+
+# allow no password ssh
+# echo "adding to authorized_keys on server2"
+cat /vagrant/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+
+# cleanup the shared key files
+rm -rf /vagrant/id_rsa*
